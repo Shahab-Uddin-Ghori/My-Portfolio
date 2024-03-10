5bc498cf292c4e8e2d4166f8c4af446f2e94fe7c
Shahab Uddin Ghori <154582528+Shahab-Uddin-Ghori@users.noreply.github.com>
2024-03-10 16:00:39 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -63,7 +63,7 @@ ScrollReveal().reveal('.home-content p, .about-content',
 /*==================== typed js ====================*/
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Mern Developer', 'Freelancer', 'Teacher','and a Youtuber'],
+    strings: ['Mern Developer', 'Freelancer', 'Teacher','Youtuber'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
